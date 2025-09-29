b5608ab9a6eeccb44861c5c65866790e6856c54f
Avi7620 <114797861+Avi7620@users.noreply.github.com>
2025-09-29 16:48:35 +0530

    intial updated

--- emailService.py
+++ emailService.py
@@ -12,8 +12,8 @@ def init_mail(app):
     app.config["MAIL_SERVER"] = "smtp.gmail.com"
     app.config["MAIL_PORT"] = 587
     app.config["MAIL_USE_TLS"] = True
-    app.config["MAIL_USERNAME"] = app.config.get("jadhavavi7620@gmail.com")  # Set in environment
-    app.config["MAIL_PASSWORD"] = app.config.get("pfwhfzhxcucbcoiy")  # Gmail App Password
+    app.config["MAIL_USERNAME"] = app.config.get("MAIL_USERNAME")  # Set in environment
+    app.config["MAIL_PASSWORD"] = app.config.get("MAIL_PASSWORD")  # Gmail App Password
     mail.init_app(app)
 
 def send_otp_email(email):
